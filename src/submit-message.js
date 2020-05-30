b637aa5605a95fb2b29bc5ace038e55b4ace4793
Noah Schneider <noahlschneider@gmail.com>
2020-05-30 11:29:02 -0500

    Formatting

--- src/submit-message.js
+++ src/submit-message.js
@@ -12,15 +12,14 @@ module.exports.submitMessage =  async function(submitKey, topicId, message, clie
         throw new Error("environment variables OPERATOR_KEY and OPERATOR_ID must be present");
     }
 	
-	
 	var message = await new ConsensusMessageSubmitTransaction()
 		 .setTopicId(topicId)
 		 .setMessage(message)
 		 .build(client)
-		 // The transaction is automatically signed by the payer.
 		 // Due to the topic having a submitKey requirement, additionally sign the transaction with that key.
 		.sign(submitKey)
 		.execute(client)
+		//This didn't work for some reason...
 		//.getReceipt(client);
 		
 	return message;
