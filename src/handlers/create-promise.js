e7193f9333fed243a87cc46b83ed4ddc237dd5e9
peterfc@gmail.com <peterfc@gmail.com>
2020-06-06 14:25:28 -0500

    added full promise to initial create

--- src/handlers/create-promise.js
+++ src/handlers/create-promise.js
@@ -3,6 +3,7 @@
 const utils = require("../utils");
 const cpt = require("../create-private-topic");
 const sm = require("../submit-message");
+const pb = require("../promise-builder");
 const { Client, ConsensusMessageSubmitTransaction, Ed25519PrivateKey, Ed25519PublicKey} = require("@hashgraph/sdk");
 const operatorAccountId = process.env.OPERATOR_ID;
 const operatorPrivateKey = process.env.OPERATOR_KEY;
@@ -22,21 +23,58 @@ module.exports.handler = async (context, req) => {
 	client.setOperator(operatorAccountId, operatorPrivateKey);
 	
 	//Create topic for promise
-	const submitKey = await Ed25519PrivateKey.generate();
-	
-	myTopic = await cpt.createPrivateTopic(submitKey);
-	
+	const submitKey = await Ed25519PrivateKey.generate();	
+	myTopic = await cpt.createPrivateTopic(submitKey);	
 	const topicId = myTopic.topicId;
-	const message = "Hello world";
-	
-	myMessage = await sm.submitMessage(submitKey, topicId, message, client);
-	
-	context.res = { status: 200, 
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: { myTopic, myMessage } 
+
+	reqByParty = utils.getQueryOrBodyParam(req, "byParty");
+	var byParty = {};
+	if(reqByParty){
+		byParty.party_id = reqByParty;
 	}
+
+	var toParties = [];
+	var reqToParties = utils.getQueryOrBodyParam(req, "toParties");
+	if(reqToParties){
+		var reqToPartiesArr = reqToParties.split(",");
+		reqToPartiesArr.forEach(x => {
+			toParties.push({
+				party_id: x
+			});
+		});
+	}	
+
+	var onBehalfParties = [];
+	var reqOnBehalfParties = utils.getQueryOrBodyParam(req, "onBehalfParties");
+	if(reqOnBehalfParties){
+		var reqOnBehalfPartiesArr = reqOnBehalfParties.split(",");
+		reqOnBehalfPartiesArr.forEach(x => {
+			onBehalfParties.push({
+				party_id: x
+			});
+		});
+	}
+
+	var reqObligation = utils.getQueryOrBodyParam(req, "ob");
+	var promiseModel = pb.buildNewPromise(byParty, toParties, onBehalfParties, reqObligation);
+	console.log(JSON.stringify(promiseModel));
+	if(promiseModel){	
+		myMessage = await sm.submitMessage(submitKey, topicId, promiseModel, client);
+
+		context.res = { status: 200, 
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: { myTopic, myMessage, promiseModel } 
+		}
+	} else {
+		context.res = { status: 200, 
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: { myTopic, promiseModel } 
+		}
+	}		
 };
 
 
---
+++ src/promise-builder.js
@@ -0,0 +1,54 @@
+
+module.exports.buildNewPromise = (byParty, toParties, onBehalfOfParties, obligation) => {
+	
+	console.log("Build new promise");
+	if(!byParty || !byParty.party_id){
+		return undefined;
+	}
+
+	var establishedDate = new Date();
+	var expriationDate = new Date(establishedDate.getTime());
+	expriationDate.setFullYear(expriationDate.getFullYear() + 1);
+
+	var model = {
+		promise_id: "",
+		established: establishedDate.toUTCString(),
+		concluded: "",
+		status: "2",
+		expriation: expriationDate.toUTCString(),
+		description: "",
+		notes: "",
+		parties: [
+			//{
+			//	party_id:"",
+			//	name:"",
+			//	notes:""
+			//}
+		],
+		obligations: [
+			{
+				id: "1",
+				obligation: obligation,
+				by_party: byParty.party_id,
+				to_parties: [],
+				on_behalf_of: [],
+				attestations: [],
+				status: "2",
+				conditions: "",
+				attachments: [],
+				expriation: expriationDate.toUTCString(),
+				dependent_on: []
+			}
+		]
+	};
+
+	model.parties.push(byParty);
+	model.parties = model.parties.concat(toParties);
+	model.parties = model.parties.concat(onBehalfOfParties);
+
+	model.obligations[0].to_parties = model.obligations[0].to_parties.concat(toParties.map(x => x.party_id));
+
+	model.obligations[0].on_behalf_of = model.obligations[0].on_behalf_of.concat(onBehalfOfParties.map(x => x.party_id));
+
+	return model;
+}
